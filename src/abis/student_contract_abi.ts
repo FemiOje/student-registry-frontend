62ee36412eb1e60809abf1ae1538ccf1077a8b3e
Femi Ojetokun <ojetokunakinwale@gmail.com>
2024-11-30 17:15:40 +0100

    update ABI and contract address

--- src/abis/student_contract_abi.ts
+++ src/abis/student_contract_abi.ts
@@ -2,192 +2,361 @@ import { type Abi } from "starknet";
 
 
 export const student_contract_abi = [
-    {
-      "type": "impl",
-      "name": "StudentRegistryImpl",
-      "interface_name": "contracts::student_registry::IStudentRegistry"
-    },
-    {
-      "type": "enum",
-      "name": "core::bool",
-      "variants": [
-        {
-          "name": "False",
-          "type": "()"
-        },
-        {
-          "name": "True",
-          "type": "()"
-        }
-      ]
-    },
-    {
-      "type": "struct",
-      "name": "contracts::student_struct::Student",
-      "members": [
-        {
-          "name": "id",
-          "type": "core::integer::u64"
-        },
-        {
-          "name": "fname",
-          "type": "core::felt252"
-        },
-        {
-          "name": "lname",
-          "type": "core::felt252"
-        },
-        {
-          "name": "phone_number",
-          "type": "core::felt252"
-        },
-        {
-          "name": "age",
-          "type": "core::integer::u8"
-        },
-        {
-          "name": "is_active",
-          "type": "core::bool"
-        }
-      ]
-    },
-    {
-      "type": "struct",
-      "name": "core::array::Span::<contracts::student_struct::Student>",
-      "members": [
-        {
-          "name": "snapshot",
-          "type": "@core::array::Array::<contracts::student_struct::Student>"
-        }
-      ]
-    },
-    {
-      "type": "interface",
-      "name": "contracts::student_registry::IStudentRegistry",
-      "items": [
-        {
-          "type": "function",
-          "name": "add_student",
-          "inputs": [
-            {
-              "name": "_fname",
-              "type": "core::felt252"
-            },
-            {
-              "name": "_lname",
-              "type": "core::felt252"
-            },
-            {
-              "name": "_phone_number",
-              "type": "core::felt252"
-            },
-            {
-              "name": "_age",
-              "type": "core::integer::u8"
-            },
-            {
-              "name": "_is_active",
-              "type": "core::bool"
-            }
-          ],
-          "outputs": [
-            {
-              "type": "core::bool"
-            }
-          ],
-          "state_mutability": "external"
-        },
-        {
-          "type": "function",
-          "name": "get_student",
-          "inputs": [
-            {
-              "name": "index",
-              "type": "core::integer::u64"
-            }
-          ],
-          "outputs": [
-            {
-              "type": "(core::felt252, core::felt252, core::felt252, core::integer::u8, core::bool)"
-            }
-          ],
-          "state_mutability": "view"
-        },
-        {
-          "type": "function",
-          "name": "get_all_students",
-          "inputs": [],
-          "outputs": [
-            {
-              "type": "core::array::Span::<contracts::student_struct::Student>"
-            }
-          ],
-          "state_mutability": "view"
-        },
-        {
-          "type": "function",
-          "name": "update_student",
-          "inputs": [
-            {
-              "name": "_index",
-              "type": "core::integer::u64"
-            },
-            {
-              "name": "_fname",
-              "type": "core::felt252"
-            },
-            {
-              "name": "_lname",
-              "type": "core::felt252"
-            },
-            {
-              "name": "_phone_number",
-              "type": "core::felt252"
-            },
-            {
-              "name": "_age",
-              "type": "core::integer::u8"
-            }
-          ],
-          "outputs": [
-            {
-              "type": "core::bool"
-            }
-          ],
-          "state_mutability": "external"
-        },
-        {
-          "type": "function",
-          "name": "delete_student",
-          "inputs": [
-            {
-              "name": "_index",
-              "type": "core::integer::u64"
-            }
-          ],
-          "outputs": [
-            {
-              "type": "core::bool"
-            }
-          ],
-          "state_mutability": "external"
-        }
-      ]
-    },
-    {
-      "type": "constructor",
-      "name": "constructor",
-      "inputs": [
-        {
-          "name": "_admin",
-          "type": "core::starknet::contract_address::ContractAddress"
-        }
-      ]
-    },
-    {
-      "type": "event",
-      "name": "contracts::student_registry::StudentRegistry::Event",
-      "kind": "enum",
-      "variants": []
-    }
-  ] as const satisfies Abi;
+  {
+    "name": "UpgradeableImpl",
+    "type": "impl",
+    "interface_name": "openzeppelin_upgrades::interface::IUpgradeable"
+  },
+  {
+    "name": "openzeppelin_upgrades::interface::IUpgradeable",
+    "type": "interface",
+    "items": [
+      {
+        "name": "upgrade",
+        "type": "function",
+        "inputs": [
+          {
+            "name": "new_class_hash",
+            "type": "core::starknet::class_hash::ClassHash"
+          }
+        ],
+        "outputs": [],
+        "state_mutability": "external"
+      }
+    ]
+  },
+  {
+    "name": "StudentRegistryImpl",
+    "type": "impl",
+    "interface_name": "student_registry_contract::student_registry::IStudentRegistry"
+  },
+  {
+    "name": "core::bool",
+    "type": "enum",
+    "variants": [
+      {
+        "name": "False",
+        "type": "()"
+      },
+      {
+        "name": "True",
+        "type": "()"
+      }
+    ]
+  },
+  {
+    "name": "student_registry_contract::student_struct::Student",
+    "type": "struct",
+    "members": [
+      {
+        "name": "id",
+        "type": "core::integer::u64"
+      },
+      {
+        "name": "fname",
+        "type": "core::felt252"
+      },
+      {
+        "name": "lname",
+        "type": "core::felt252"
+      },
+      {
+        "name": "phone_number",
+        "type": "core::felt252"
+      },
+      {
+        "name": "age",
+        "type": "core::integer::u8"
+      },
+      {
+        "name": "is_active",
+        "type": "core::bool"
+      }
+    ]
+  },
+  {
+    "name": "core::array::Span::<student_registry_contract::student_struct::Student>",
+    "type": "struct",
+    "members": [
+      {
+        "name": "snapshot",
+        "type": "@core::array::Array::<student_registry_contract::student_struct::Student>"
+      }
+    ]
+  },
+  {
+    "name": "student_registry_contract::student_registry::IStudentRegistry",
+    "type": "interface",
+    "items": [
+      {
+        "name": "add_student",
+        "type": "function",
+        "inputs": [
+          {
+            "name": "fname",
+            "type": "core::felt252"
+          },
+          {
+            "name": "lname",
+            "type": "core::felt252"
+          },
+          {
+            "name": "phone_number",
+            "type": "core::felt252"
+          },
+          {
+            "name": "age",
+            "type": "core::integer::u8"
+          },
+          {
+            "name": "is_active",
+            "type": "core::bool"
+          }
+        ],
+        "outputs": [
+          {
+            "type": "core::bool"
+          }
+        ],
+        "state_mutability": "external"
+      },
+      {
+        "name": "get_student",
+        "type": "function",
+        "inputs": [
+          {
+            "name": "index",
+            "type": "core::integer::u64"
+          }
+        ],
+        "outputs": [
+          {
+            "type": "(core::felt252, core::felt252, core::felt252, core::integer::u8, core::bool)"
+          }
+        ],
+        "state_mutability": "view"
+      },
+      {
+        "name": "get_all_students",
+        "type": "function",
+        "inputs": [],
+        "outputs": [
+          {
+            "type": "core::array::Span::<student_registry_contract::student_struct::Student>"
+          }
+        ],
+        "state_mutability": "view"
+      },
+      {
+        "name": "update_student",
+        "type": "function",
+        "inputs": [
+          {
+            "name": "index",
+            "type": "core::integer::u64"
+          },
+          {
+            "name": "fname",
+            "type": "core::felt252"
+          },
+          {
+            "name": "lname",
+            "type": "core::felt252"
+          },
+          {
+            "name": "phone_number",
+            "type": "core::felt252"
+          },
+          {
+            "name": "age",
+            "type": "core::integer::u8"
+          }
+        ],
+        "outputs": [
+          {
+            "type": "core::bool"
+          }
+        ],
+        "state_mutability": "external"
+      },
+      {
+        "name": "delete_student",
+        "type": "function",
+        "inputs": [
+          {
+            "name": "_index",
+            "type": "core::integer::u64"
+          }
+        ],
+        "outputs": [
+          {
+            "type": "core::bool"
+          }
+        ],
+        "state_mutability": "external"
+      }
+    ]
+  },
+  {
+    "name": "OwnableMixinImpl",
+    "type": "impl",
+    "interface_name": "openzeppelin_access::ownable::interface::OwnableABI"
+  },
+  {
+    "name": "openzeppelin_access::ownable::interface::OwnableABI",
+    "type": "interface",
+    "items": [
+      {
+        "name": "owner",
+        "type": "function",
+        "inputs": [],
+        "outputs": [
+          {
+            "type": "core::starknet::contract_address::ContractAddress"
+          }
+        ],
+        "state_mutability": "view"
+      },
+      {
+        "name": "transfer_ownership",
+        "type": "function",
+        "inputs": [
+          {
+            "name": "new_owner",
+            "type": "core::starknet::contract_address::ContractAddress"
+          }
+        ],
+        "outputs": [],
+        "state_mutability": "external"
+      },
+      {
+        "name": "renounce_ownership",
+        "type": "function",
+        "inputs": [],
+        "outputs": [],
+        "state_mutability": "external"
+      },
+      {
+        "name": "transferOwnership",
+        "type": "function",
+        "inputs": [
+          {
+            "name": "newOwner",
+            "type": "core::starknet::contract_address::ContractAddress"
+          }
+        ],
+        "outputs": [],
+        "state_mutability": "external"
+      },
+      {
+        "name": "renounceOwnership",
+        "type": "function",
+        "inputs": [],
+        "outputs": [],
+        "state_mutability": "external"
+      }
+    ]
+  },
+  {
+    "name": "constructor",
+    "type": "constructor",
+    "inputs": [
+      {
+        "name": "_admin",
+        "type": "core::starknet::contract_address::ContractAddress"
+      }
+    ]
+  },
+  {
+    "kind": "struct",
+    "name": "openzeppelin_access::ownable::ownable::OwnableComponent::OwnershipTransferred",
+    "type": "event",
+    "members": [
+      {
+        "kind": "key",
+        "name": "previous_owner",
+        "type": "core::starknet::contract_address::ContractAddress"
+      },
+      {
+        "kind": "key",
+        "name": "new_owner",
+        "type": "core::starknet::contract_address::ContractAddress"
+      }
+    ]
+  },
+  {
+    "kind": "struct",
+    "name": "openzeppelin_access::ownable::ownable::OwnableComponent::OwnershipTransferStarted",
+    "type": "event",
+    "members": [
+      {
+        "kind": "key",
+        "name": "previous_owner",
+        "type": "core::starknet::contract_address::ContractAddress"
+      },
+      {
+        "kind": "key",
+        "name": "new_owner",
+        "type": "core::starknet::contract_address::ContractAddress"
+      }
+    ]
+  },
+  {
+    "kind": "enum",
+    "name": "openzeppelin_access::ownable::ownable::OwnableComponent::Event",
+    "type": "event",
+    "variants": [
+      {
+        "kind": "nested",
+        "name": "OwnershipTransferred",
+        "type": "openzeppelin_access::ownable::ownable::OwnableComponent::OwnershipTransferred"
+      },
+      {
+        "kind": "nested",
+        "name": "OwnershipTransferStarted",
+        "type": "openzeppelin_access::ownable::ownable::OwnableComponent::OwnershipTransferStarted"
+      }
+    ]
+  },
+  {
+    "kind": "struct",
+    "name": "openzeppelin_upgrades::upgradeable::UpgradeableComponent::Upgraded",
+    "type": "event",
+    "members": [
+      {
+        "kind": "data",
+        "name": "class_hash",
+        "type": "core::starknet::class_hash::ClassHash"
+      }
+    ]
+  },
+  {
+    "kind": "enum",
+    "name": "openzeppelin_upgrades::upgradeable::UpgradeableComponent::Event",
+    "type": "event",
+    "variants": [
+      {
+        "kind": "nested",
+        "name": "Upgraded",
+        "type": "openzeppelin_upgrades::upgradeable::UpgradeableComponent::Upgraded"
+      }
+    ]
+  },
+  {
+    "kind": "enum",
+    "name": "student_registry_contract::student_registry::StudentRegistry::Event",
+    "type": "event",
+    "variants": [
+      {
+        "kind": "flat",
+        "name": "OwnableEvent",
+        "type": "openzeppelin_access::ownable::ownable::OwnableComponent::Event"
+      },
+      {
+        "kind": "flat",
+        "name": "UpgradeableEvent",
+        "type": "openzeppelin_upgrades::upgradeable::UpgradeableComponent::Event"
+      }
+    ]
+  }
+] as const satisfies Abi;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,108 +1,3 @@
-// import { useStarknetkitConnectModal, StarknetkitConnector } from "starknetkit";
-// import { useState, useEffect } from "react";
-// import { useAccount, useConnect, useDisconnect, Connector } from "@starknet-react/core";
-// import { availableConnectors } from "../helpers/connectors";
-// import toast from "react-hot-toast";
-
-// export default function Header() {
-//   const [dropdownOpen, setDropdownOpen] = useState(false);
-//   const { address } = useAccount();
-//   const { connect } = useConnect();
-
-//   useEffect(() => {
-//     const storedConnector = sessionStorage.getItem("connector");
-//     if (storedConnector && !address) {
-//       const parsedConnector = JSON.parse(storedConnector);
-//       const connectorInstance = availableConnectors.find(
-//         (c) => c.id === parsedConnector.id
-//       ) as Connector;
-
-//       if (connectorInstance) {
-//         try {
-//           connect({ connector: connectorInstance });          
-//         } catch (error) {
-//           console.error("Reconnect failed:", error);
-//           sessionStorage.removeItem("connector");
-//         }
-//       }
-//     }
-//   }, [address, connect]);
-  
-//   const { disconnect } = useDisconnect({});
-//   const { starknetkitConnectModal } = useStarknetkitConnectModal({
-//     connectors: availableConnectors as StarknetkitConnector[],
-//   });
-
-//   async function connectWalletWithModal() {
-//     try {
-//       const { connector } = await starknetkitConnectModal();
-
-//       if (!connector) {
-//         toast.error("No wallet selected.");
-//         return;
-//       }
-
-//       await connect({ connector: connector as Connector });
-//       toast.success("Connected successfully");
-
-//       sessionStorage.setItem("connector", JSON.stringify({ id: connector.id }));
-//     } catch (error) {
-//       toast.error("Error occurred. " + error + " Please try again.");
-//     }
-//   }
-
-//   const disconnectWallet = async () => {
-//     try {
-//       setDropdownOpen(false);
-//       await disconnect();
-//       sessionStorage.removeItem("connector");
-//       toast.success("Wallet disconnected");
-//     } catch (error) {
-//       toast.error("Error occurred." + error + " Please try again.");
-//     }
-//   };
-
-//   return (
-//     <nav className="flex items-center justify-between sticky top-0 shadow-lg p-5 bg-white">
-//       <div className="title text-xl font-semibold">Student Registry</div>
-//       <div className="wallet-status ml-auto relative">
-//         {address ? (
-//           <div className="flex items-center">
-//             <p className="text-black bg-gray-200 rounded-full px-5 py-1">
-//               {address?.slice(0, 6)}...
-//               {address?.slice(-4)}
-//             </p>
-//             <button
-//               onClick={() => setDropdownOpen((prev) => !prev)}
-//               className="ml-2 text-lg focus:outline-none"
-//             >
-//               ▼
-//             </button>
-//             {dropdownOpen && (
-//               <div className="absolute right-0 mt-2 w-32 bg-white border rounded shadow-lg z-10">
-//                 <button
-//                   onClick={() => disconnectWallet()}
-//                   className="w-full px-4 py-5 text-center text-red-600 hover:bg-red-100"
-//                 >
-//                   Disconnect
-//                 </button>
-//               </div>
-//             )}
-//           </div>
-//         ) : (
-//           <button
-//             className="bg-blue-700 text-white rounded-full px-4 py-2 hover:bg-blue-600 transition"
-//             onClick={() => connectWalletWithModal()}
-//           >
-//             Connect Wallet
-//           </button>
-//         )}
-//       </div>
-//     </nav>
-//   );
-// }
-
-
 import { useStarknetkitConnectModal, StarknetkitConnector } from "starknetkit";
 import { useState, useEffect } from "react";
 import { useAccount, useConnect, useDisconnect, Connector } from "@starknet-react/core";
@@ -112,59 +7,47 @@ import toast from "react-hot-toast";
 export default function Header() {
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const { address } = useAccount();
-  const { connect, connectors } = useConnect();
-  
-  const { disconnect } = useDisconnect({});
-  
-  const { starknetkitConnectModal } = useStarknetkitConnectModal({
-    connectors: availableConnectors as StarknetkitConnector[],
-  });
+  const { connect } = useConnect();
 
   useEffect(() => {
     const storedConnector = sessionStorage.getItem("connector");
     if (storedConnector && !address) {
-      try {
-        const parsedConnector = JSON.parse(storedConnector);
-    
-        const matchingConnector = connectors.find(
-          (c) => c.id === parsedConnector.id
-        );
+      const parsedConnector = JSON.parse(storedConnector);
+      const connectorInstance = availableConnectors.find(
+        (c) => c.id === parsedConnector.id
+      ) as Connector;
 
-        if (matchingConnector) {
-          connect({ connector: matchingConnector });
+      if (connectorInstance) {
+        try {
+          connect({ connector: connectorInstance });          
+        } catch (error) {
+          console.error("Reconnect failed:", error);
+          sessionStorage.removeItem("connector");
         }
-      } catch (error) {
-        console.error("Reconnect failed:", error);
-        sessionStorage.removeItem("connector");
       }
     }
-  }, [address, connect, connectors]);
+  }, [address, connect]);
+  
+  const { disconnect } = useDisconnect({});
+  const { starknetkitConnectModal } = useStarknetkitConnectModal({
+    connectors: availableConnectors as StarknetkitConnector[],
+  });
 
   async function connectWalletWithModal() {
     try {
-      const result = await starknetkitConnectModal();
-      
-      if (!result?.connector) {
+      const { connector } = await starknetkitConnectModal();
+
+      if (!connector) {
         toast.error("No wallet selected.");
         return;
       }
 
-      const matchingConnector = connectors.find(
-        (c) => c.id === result?.connector?.id
-      );
+      await connect({ connector: connector as Connector });
+      toast.success("Connected successfully");
 
-      if (matchingConnector) {
-        await connect({ connector: matchingConnector });
-        toast.success("Connected successfully");
-        
-        sessionStorage.setItem("connector", JSON.stringify({ 
-          id: result.connector.id 
-        }));
-      } else {
-        toast.error("Could not find matching connector");
-      }
+      sessionStorage.setItem("connector", JSON.stringify({ id: connector.id }));
     } catch (error) {
-      toast.error("Error occurred. " + String(error) + " Please try again.");
+      toast.error("Error occurred. " + error + " Please try again.");
     }
   }
 
@@ -175,7 +58,7 @@ export default function Header() {
       sessionStorage.removeItem("connector");
       toast.success("Wallet disconnected");
     } catch (error) {
-      toast.error("Error occurred." + String(error) + " Please try again.");
+      toast.error("Error occurred." + error + " Please try again.");
     }
   };
 
@@ -218,3 +101,120 @@ export default function Header() {
     </nav>
   );
 }
+
+
+// import { useStarknetkitConnectModal, StarknetkitConnector } from "starknetkit";
+// import { useState, useEffect } from "react";
+// import { useAccount, useConnect, useDisconnect, Connector } from "@starknet-react/core";
+// import { availableConnectors } from "../helpers/connectors";
+// import toast from "react-hot-toast";
+
+// export default function Header() {
+//   const [dropdownOpen, setDropdownOpen] = useState(false);
+//   const { address } = useAccount();
+//   const { connect, connectors } = useConnect();
+  
+//   const { disconnect } = useDisconnect({});
+  
+//   const { starknetkitConnectModal } = useStarknetkitConnectModal({
+//     connectors: availableConnectors as StarknetkitConnector[],
+//   });
+
+//   useEffect(() => {
+//     const storedConnector = sessionStorage.getItem("connector");
+//     if (storedConnector && !address) {
+//       try {
+//         const parsedConnector = JSON.parse(storedConnector);
+    
+//         const matchingConnector = connectors.find(
+//           (c) => c.id === parsedConnector.id
+//         );
+
+//         if (matchingConnector) {
+//           connect({ connector: matchingConnector });
+//         }
+//       } catch (error) {
+//         console.error("Reconnect failed:", error);
+//         sessionStorage.removeItem("connector");
+//       }
+//     }
+//   }, [address, connect, connectors]);
+
+//   async function connectWalletWithModal() {
+//     try {
+//       const result = await starknetkitConnectModal();
+      
+//       if (!result?.connector) {
+//         toast.error("No wallet selected.");
+//         return;
+//       }
+
+//       const matchingConnector = connectors.find(
+//         (c) => c.id === result?.connector?.id
+//       );
+
+//       if (matchingConnector) {
+//         await connect({ connector: matchingConnector });
+//         toast.success("Connected successfully");
+        
+//         sessionStorage.setItem("connector", JSON.stringify({ 
+//           id: result.connector.id 
+//         }));
+//       } else {
+//         toast.error("Could not find matching connector");
+//       }
+//     } catch (error) {
+//       toast.error("Error occurred. " + String(error) + " Please try again.");
+//     }
+//   }
+
+//   const disconnectWallet = async () => {
+//     try {
+//       setDropdownOpen(false);
+//       await disconnect();
+//       sessionStorage.removeItem("connector");
+//       toast.success("Wallet disconnected");
+//     } catch (error) {
+//       toast.error("Error occurred." + String(error) + " Please try again.");
+//     }
+//   };
+
+//   return (
+//     <nav className="flex items-center justify-between sticky top-0 shadow-lg p-5 bg-white">
+//       <div className="title text-xl font-semibold">Student Registry</div>
+//       <div className="wallet-status ml-auto relative">
+//         {address ? (
+//           <div className="flex items-center">
+//             <p className="text-black bg-gray-200 rounded-full px-5 py-1">
+//               {address?.slice(0, 6)}...
+//               {address?.slice(-4)}
+//             </p>
+//             <button
+//               onClick={() => setDropdownOpen((prev) => !prev)}
+//               className="ml-2 text-lg focus:outline-none"
+//             >
+//               ▼
+//             </button>
+//             {dropdownOpen && (
+//               <div className="absolute right-0 mt-2 w-32 bg-white border rounded shadow-lg z-10">
+//                 <button
+//                   onClick={() => disconnectWallet()}
+//                   className="w-full px-4 py-5 text-center text-red-600 hover:bg-red-100"
+//                 >
+//                   Disconnect
+//                 </button>
+//               </div>
+//             )}
+//           </div>
+//         ) : (
+//           <button
+//             className="bg-blue-700 text-white rounded-full px-4 py-2 hover:bg-blue-600 transition"
+//             onClick={() => connectWalletWithModal()}
+//           >
+//             Connect Wallet
+//           </button>
+//         )}
+//       </div>
+//     </nav>
+//   );
+// }
